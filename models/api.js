366e33e8aa49d7a19a31d39a5c80dd09f8d293dc
ricmtz <ricmtz15@gmail.com>
2018-12-02 01:34:03 -0600

    #21 fetch package installed and basic estructure for API methods

--- models/api.js
+++ models/api.js
@@ -1,6 +1,24 @@
+const fetch = require('node-fetch');
 
 class API {
+    async getMethod(url='', options={}) {
+        fetch('https://rickandmortyapi.com/api/character/')
+            .then(res => res.json())
+            .then((res) => { console.log(res); })
+            .catch(err => console.log(err));
+    }
 
+    async postMethod(url, options) {
+
+    }
+
+    async patchMethod(url, options) {
+
+    }
+
+    async delMethod(url, options) {
+
+    }
 }
 
 module.exports = new API();
