9a255ef9cf6c9d29e13e14c6619bb68d6ab3196f
ricmtz <ricmtz15@gmail.com>
2018-12-01 23:26:59 -0600

    #21 views modified to use partials

--- controllers/game.js
+++ controllers/game.js
@@ -2,7 +2,44 @@ const fs = require('fs');
 const Mustache = require('mustache');
 
 class Game {
+    async selectAdversaryPage(req, res) {
+        const template = fs.readFileSync('public/views/select_adversary.mst').toString();
+        const menu = fs.readFileSync('public/partials/menu.mst').toString();
+        const friends = fs.readFileSync('public/partials/friend_list.mst').toString();
+        const footer = fs.readFileSync('public/partials/footer.mst').toString();
+        const data = {
+            nickname: req.cookies.nickname,
+            adversary: true,
+            friends: [
+                {
+                    img: 'https://www.enriquedans.com/wp-content/uploads/2018/06/GitHub-Octocat.jpg',
+                    nickname: 'Asasdas_asdad',
+                },
+                {
+                    img: 'https://www.enriquedans.com/wp-content/uploads/2018/06/GitHub-Octocat.jpg',
+                    nickname: 'Asasdas_asdad',
+                },
+                {
+                    img: 'https://www.enriquedans.com/wp-content/uploads/2018/06/GitHub-Octocat.jpg',
+                    nickname: 'Asasdas_asdad',
+                }
+            ]
+        
+        };
+        const html = Mustache.to_html(template, data, { menu, footer, friends });
+        res.send(html);
+    }
 
+    async roulettePage(req, res) {
+        const template = fs.readFileSync('public/views/roulette.mst').toString();
+        const menu = fs.readFileSync('public/partials/menu.mst').toString();
+        const footer = fs.readFileSync('public/partials/footer.mst').toString();
+        const data = {
+            nickname: req.cookies.nickname,
+        };
+        const html = Mustache.to_html(template, data, { menu, footer });
+        res.send(html);
+    }
 }
 
 module.exports = new Game();
