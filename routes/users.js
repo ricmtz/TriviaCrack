0d9e418513fc9c1189f8c3500becda9f3dd132f2
ricmtz <ricmtz15@gmail.com>
2018-12-01 21:39:59 -0600

    #21 list of emails and friends partials implemented

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ const { CtrlUser } = require('../controllers');
 const router = Router();
 
 router.get('/login', CtrlUser.loginPage);
-
 router.get('/register', CtrlUser.registerPage);
+router.get('/profile', CtrlUser.profilePage);
+router.post('/profile', function(req, res){
+    res.send('Not implemented')
+});
+router.get('/profile/edit', CtrlUser.profileEditPage);
+router.get('/profile/add_emails', CtrlUser.addEmailsPage);
 
 module.exports = router;
