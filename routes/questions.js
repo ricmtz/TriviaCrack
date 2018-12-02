affe881523da4f72223e896580fe6ceb2fcb7eed
ricmtz <ricmtz15@gmail.com>
2018-12-02 00:12:32 -0600

    #21 question view implemented as mustache template

--- routes/questions.js
+++ routes/questions.js
@@ -2,5 +2,6 @@ const { Router } = require('express');
 const { CtrlQuestion } = require('../controllers');
 const router = Router();
 
+router.get('/question', CtrlQuestion.questionPage);
 
 module.exports = router;
