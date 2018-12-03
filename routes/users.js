d94fa8186293f52f7b4ff3b901dc2a4a6c15a128
Adriana Castañeda <castalozano.adrigpe@gmail.com>
2018-12-03 12:50:35 -0600

    #21 modified route of login and register

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const { Router } = require('express');
 const { CtrlUser } = require('../controllers');
 const router = Router();
 
-router.get('/login', CtrlUser.loginPage);
-router.get('/register', CtrlUser.registerPage);
+router.get('/#login', CtrlUser.loginPage);
+router.get('/#singin', CtrlUser.registerPage);
 router.get('/profile', CtrlUser.profilePage);
 router.post('/profile', function(req, res){
     res.send('Not implemented')
