a6793cd4c683abcbeb0aa3c272d9b4f6eda4b40e
ricmtz <ricmtz15@gmail.com>
2018-12-03 17:41:06 -0600

    #21 error with duplicate values soved

--- app/js/principal.js
+++ app/js/principal.js
@@ -5,29 +5,33 @@ function show() {
     menu.classList.toggle('hide');
 }
 
-let hide;
+let hideHead;
 function headerScroll() {
-    document.getElementById('header').classList.remove('no-visible');
-    window.clearTimeout(hide);
-    const header = document.getElementsByTagName('header');
-    const logo = document.getElementById('logo');
-    const options = document.getElementById('drop-menu');
-    const menu = document.getElementById('menu');
-    if (window.scrollY <= 100){
-        clearTimeout(hide);
-        header[0].classList.remove('scroller');
-        options.classList.remove('scroller');
-        menu.classList.remove('scroller');
-        menu.parentNode.classList.add('menu');
-        itemMenuScroll(false);
-    }
-    else {
-        header[0].classList.add('scroller');
-        options.classList.add('scroller');
-        menu.classList.add('scroller');
-        menu.parentNode.classList.remove('menu');
-        itemMenuScroll(true);
-        hide = setTimeout(hideHeader,ITME_HIDE_HEADER);
+    try {
+        document.getElementById('header').classList.remove('no-visible');
+        window.clearTimeout(hideHead);
+        const header = document.getElementsByTagName('header');
+        const logo = document.getElementById('logo');
+        const options = document.getElementById('drop-menu');
+        const menu = document.getElementById('menu');
+        if (window.scrollY <= 100) {
+            clearTimeout(hideHead);
+            header[0].classList.remove('scroller');
+            options.classList.remove('scroller');
+            menu.classList.remove('scroller');
+            menu.parentNode.classList.add('menu');
+            itemMenuScroll(false);
+        }
+        else {
+            header[0].classList.add('scroller');
+            options.classList.add('scroller');
+            menu.classList.add('scroller');
+            menu.parentNode.classList.remove('menu');
+            itemMenuScroll(true);
+            hideHead = setTimeout(hideHeader, ITME_HIDE_HEADER);
+        }
+    } catch (error) {
+
     }
 }
 
@@ -35,7 +39,7 @@ function hideHeader() {
     document.getElementById('header').classList.add('no-visible');
 }
 
-function itemMenuScroll(add){
+function itemMenuScroll(add) {
     const items = document.getElementsByTagName('a');
     for (let i of items) {
         if (i.parentNode.parentNode.id === 'menu') {
@@ -49,7 +53,7 @@ function itemMenuScroll(add){
     }
 }
 
-function changePictureWelcome(){
+function changePictureWelcome() {
     const picture = document.getElementById('welcome');
     if (window.screen.width > 450) {
         picture.src = "img/welcome.png";
@@ -59,16 +63,16 @@ function changePictureWelcome(){
     }
 }
 
-function loadingPrincipal(){
+function loadingPrincipal() {
     try {
         const btn = document.getElementById('drop-menu');
         btn.addEventListener('click', show);
         changePictureWelcome();
     } catch (error) {
-        
+
     }
 }
 
 
-window.addEventListener('scroll',headerScroll);
+window.addEventListener('scroll', headerScroll);
 window.addEventListener('load', loadingPrincipal);
