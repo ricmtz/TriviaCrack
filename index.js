50deb6c1b847bc0d9bb28c4c902c031affbad256
ricmtz <ricmtz15@gmail.com>
2018-12-02 23:35:04 -0600

    #21 body parser installed and implemented

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const router = require('./routes');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', router);
 
